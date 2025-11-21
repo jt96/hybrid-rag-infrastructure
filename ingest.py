16eebe9b3f1145c3d20cceac58868ce044ccc4ed
unknown <29517154+jt96@users.noreply.github.com>
2025-11-20 20:35:40 -0800

    Add testing pinecone index script to verify

--- ingest.py
+++ ingest.py
@@ -1,7 +1,15 @@
 import os
 import sys
+from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+
+def setup_env():
+    print("Loading environment variables...")
+    load_dotenv()
+    print("Variables loaded.")
 
 def ingest_docs():
     file_path = "sample.pdf"
@@ -19,7 +27,7 @@ def ingest_docs():
     # 200 overlap ensures we don't cut a sentence in half.
     chunk_size = 1000
     chunk_overlap = 200
-
+     
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     print("Splitting documents...")
     splits = text_splitter.split_documents(docs)
@@ -32,11 +40,45 @@ def ingest_docs():
     print(f"Created {len(splits)} vector chunks.")
 
     # Print the first chunk to prove it worked
-    print(f"Content of Chunk 1: \n{splits[0].page_content[:200]}...")
+    # print(f"Content of Chunk 1: \n{splits[0].page_content[:200]}...")
+    return splits
+
+def connect_to_cloud(splits):
+    try:
+        PINECONE_INDEX_NAME = os.environ["PINECONE_INDEX_NAME"]
+    except Exception as e:
+        print(f"Environment variable missing: {e}")
+        sys.exit(1)
+
+    # Initialize the "Converter" (Text -> Numbers)
+    print("Initializing Local AI Embedding Model...")
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    # Batch Processing Logic
+    batch_size = 50  # Small batch to stay under free tier limits
+    total_chunks = len(splits)
+
+    for i in range(0, total_chunks, batch_size):
+        batch = splits[i:i+batch_size]
+        print(f"Processing batch {i} to {i+len(batch)}")
+
+        try:
+            PineconeVectorStore.from_documents(
+                documents=batch,
+                embedding=embeddings,
+                index_name=PINECONE_INDEX_NAME
+            )
+        except Exception as e:
+            print(f"Error on batch: {e}")
+            sys.exit(1)
+    
+    print("Upload complete.")
 
 def main():
     try:
-        ingest_docs()
+        setup_env()
+        text_chunks = ingest_docs()
+        connect_to_cloud(text_chunks)
     except Exception as e:
         print(f"Failed: {e}")
         sys.exit(1)
---
+++ test_db.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from ingest import setup_env
+from langchain_pinecone import PineconeVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+def test_index():
+    try:
+        PINECONE_INDEX_NAME = os.environ["PINECONE_INDEX_NAME"]
+    except Exception as e:
+        print(f"Environment variable missing: {e}")
+        sys.exit(1)
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vector_store = PineconeVectorStore(
+        embedding=embeddings,
+        index_name=PINECONE_INDEX_NAME)
+    query = "legal advice"
+    results = vector_store.similarity_search(query=query)
+    for result in results:
+        print(result.page_content)
+    
+def main():
+    setup_env()
+    test_index()
+
+if __name__ == "__main__":
+    main()
