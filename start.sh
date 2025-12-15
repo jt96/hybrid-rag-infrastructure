d4ecf85c4c2542523773c7ac3eb4ccb752c43d76
unknown <29517154+jt96@users.noreply.github.com>
2025-12-14 19:37:12 -0800

    Add and remove comments, housekeeping

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 1. Check for a special Environment Variable
+# Check for a special Environment Variable
 if [ "$RUN_INGEST" = "true" ]; then
     echo "RUN_INGEST is set to true. Starting ingestion..."
     python ingest.py
@@ -11,9 +11,10 @@ if [ "$RUN_INGEST" = "true" ]; then
     echo "Ingestion complete."
 fi
 
-# 2. Always start the Chatbot afterwards
+# Launch the Streamlit application with clickable link
 echo "Starting Streamlit UI..."
 echo "--------------------------------------------------"
-echo "APP RUNNING! Click here -> http://localhost:8501"
+echo "Server Ready: http://localhost:8501"
 echo "--------------------------------------------------"
+
 streamlit run app.py --server.port=8501 --server.address=0.0.0.0
